aa9dcadc5d10ed538f87a9e1d3fa559fb19c60e3
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-29 18:31:17 +0700

    create user controler structure

--- server/controllers/controllers.api.notes.js
+++ server/controllers/controllers.api.notes.js
@@ -3,6 +3,75 @@ const User = models.User
 
 module.exports = {
     seedUser: (req, res) => {
+        User.create({
+            TempUserId: Date.now().toString(),
+            name: 'name a',
+            age: 11
+        }, {
+            TempUserId: Date.now().toString(),
+            name: 'name b',
+            age: 22
+        }, {
+            TempUserId: Date.now().toString(),
+            name: 'name c',
+            age: 22
+        }).then((data) => {
+            res.json(data)
+        }).catch((err) => {
+            res.json(err)
+        })
+    },
 
+    deleteAllUsers: (req, res) => {
+        User.destroy().then((data) => {
+            res.json(data)
+        }).catch((err) => {
+            res.json(err)
+        })
+    },
+
+    getUserById: (req, res) => {
+        User.findOne({
+            where: {
+                id: req.body.id
+            }
+        }).then((data) => {
+            res.json(data)
+        }).catch((err) => {
+            res.json(err)
+        })
+    },
+
+    updateUser: (req, res) => {
+        User.update({
+            name: req.body.name,
+            age: req.body.age
+        }, {
+            where: {
+                id: req.body.id
+            }
+        }).then(() => {
+            User.findOne({
+                where: {
+                    id: req.body.id
+                }
+            }).then((data) => {
+                res.json(data)
+            })
+        }).catch((err) => {
+            res.json(err)
+        })
+    },
+
+    deleteUser: (req, res) => {
+        User.destroy({
+            where: {
+                id: req.body.id
+            }
+        }).then((data) => {
+            res.json(data)
+        }).catch((err) => {
+            res.json(err)
+        })
     }
 }
