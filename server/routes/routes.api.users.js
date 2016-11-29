79076dd498bf0b73607a5ee42714d160ca06db7a
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-29 18:37:35 +0700

    create roter for users

--- server/routes/routes.api.users.js
+++ server/routes/routes.api.users.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var userControllers = require('../controllers/controllers.api.users')
 
+router.get('/users/seed', userControllers.seedUser)
+router.get('/users', userControllers.getAllUsers)
+router.get('/users/:id', userControllers.getUserById)
+router.post('/users', userControllers.createUser)
+router.update('/users', userControllers.updateUser)
+router.delete('/users/all', userControllers.deleteAllUsers)
+router.delete('/users', userControllers.deleteUser)
 
 module.exports = router;
