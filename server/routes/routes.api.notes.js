a80ef4f22e31727ffdf219c90d218b5e5fe7dbc0
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-29 19:51:56 +0700

    create route for notes

--- server/routes/routes.api.notes.js
+++ server/routes/routes.api.notes.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var noteControllers = require('../controllers/controllers.api.notes')
 
+router.get('/seed', noteControllers.seedUserAndNote)
+router.get('', noteControllers.getAllNotes)
+router.get('/:id', noteControllers.getNoteById)
+router.post('/', noteControllers.createNote)
+router.put('/', noteControllers.updateNote)
+router.delete('/all', noteControllers.deleteAllNotes)
+router.delete('/all', noteControllers.deleteAllUsers)
+router.delete('/', noteControllers.deleteNote)
 
 module.exports = router;
