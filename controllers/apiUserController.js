f7c5022111148a8b1c348856e5cea824ff44b89f
Septian A Tama <sfirst.tama@gmail.com>
2016-12-01 16:06:11 +0700

    user api controller skeleton

---
+++ controllers/apiUserController.js
@@ -0,0 +1,24 @@
+const Notes = require('./models/Notes');
+
+let getAllUsers = (res, req, next) => {
+
+}
+
+let getUserByUsername = (res, req, next) => {
+
+}
+
+let editUserById = (req, res, next) => {
+
+}
+
+let deleteUserById = (req, res, next) => {
+
+}
+
+module.exports = {
+  getAllUsers,
+  getUserByUsername,
+  editUserById,
+  deleteUserById
+}
