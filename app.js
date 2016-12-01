bb706f9a5172ae5522271de091d2d79347fd93a9
Septian A Tama <sfirst.tama@gmail.com>
2016-12-01 16:39:44 +0700

    auth success pass the test

--- app.js
+++ app.js
@@ -6,29 +6,69 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+const Users = require('./models/Users');
+
 const routes = require('./routes/index');
-const users = require('./routes/users');
+const auth = require('./routes/auth');
+// const apiData = require('./routes/apiData');
+// const apiDataDate = require('./routes/apiDataDate');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const cors = require('cors');
+const jwt = require('express-jwt');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1/personal-note', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Database connected!');
+  }
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(jwt({
+  secret: 'secretlalala',
+  credentialsRequired: false,
+  getToken: function fromHeaderOrQuerystring (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
+}));
+
+passport.use(new LocalStrategy(Users.authenticate()));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/auth', auth);
+// app.use('/api/data', apiData);
+// app.use('/api/datadate', apiDataDate);
+
+passport.serializeUser(Users.serializeUser());
+passport.deserializeUser(Users.deserializeUser());
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
